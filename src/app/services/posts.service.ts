02176ad9b7cde3332e9a808269327b421911b78b
Krishan Aggarwal <kakaggarwal@gmail.com>
2018-06-30 17:31:01 +0530

    posts service updated

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -5,24 +5,33 @@ import { Observable, of } from 'rxjs';
 import { Post } from '../models/post.model';
 import { NotificationService } from './notification.service';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+
   constructor(
     private http: HttpClient,
     private notificationService: NotificationService
   ) { }
 
+  private notify(message: string, type: string) {
+    this.notificationService.add(message, type);
+  }
+
   getPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+    return this.http.get<Post[]>('http://jsonplaceholder.typicode.com/posts');
   }
 
-  getLatestPosts(): Observable<Post[]> {
-    return this.getPosts();
+  updatePost(post: Post): Observable<any> {
+    return this.http.put('http://jsonplaceholder.typicode.com/posts/' + post.id, post, httpOptions);
   }
 
-  private notify(message: string) {
-    this.notificationService.add(message);
+  deletePost(post: Post): Observable<any> {
+    return this.http.delete('http://jsonplaceholder.typicode.com/posts/' + post.id);
   }
 }
