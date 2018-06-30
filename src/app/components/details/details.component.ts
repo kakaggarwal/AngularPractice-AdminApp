778c1610fcaf1620fb64d1a378f1f6a3f4d51539
Krishan Aggarwal <kakaggarwal@gmail.com>
2018-06-30 17:29:15 +0530

    details component updated

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostsService } from 'src/app/services/posts.service';
+import { Post } from 'src/app/models/post.model';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class DetailsComponent implements OnInit {
+  post: Post = new Post();
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private postService: PostsService, private location: Location) { }
 
   ngOnInit() {
+    this.getPostDetails();
   }
 
+  getPostDetails(): void {
+    const postId = +this.route.snapshot.paramMap.get('id');
+
+    this.postService.getPosts().subscribe(postData => this.post = postData.find(f => { return f.id == postId }));
+  }
+
+  savePost(): void {
+    this.postService.updatePost(this.post).subscribe(() => this.location.back());
+  }
+
+  deletePost(): void {
+    this.postService.deletePost(this.post).subscribe(() => this.location.back());
+  }
 }
